e0b9821921d5fa5cdb155b428cf3ada96560e96e
marcosagasi2003-source <marcos.agasi2003@gmail.com>
2025-10-30 16:21:15 -0300

    Update

--- StoreProcedureLimpieza.sql
+++ StoreProcedureLimpieza.sql
@@ -1,5 +1,6 @@
 USE AltosSaintJust
 GO
+
 CREATE OR ALTER PROCEDURE p_Limpiar_Estructura_CSC
     @SchemaName NVARCHAR(128) = 'csc'
 AS
@@ -8,7 +9,9 @@ BEGIN
 
     DECLARE @sql NVARCHAR(MAX) = N'';
 
-    -- Eliminar todas las FOREIGN KEYS del schema
+    -------------------------------------------------------------------
+    -- 1️⃣ Eliminar todas las FOREIGN KEYS del schema
+    -------------------------------------------------------------------
     SELECT @sql = @sql + 'ALTER TABLE [' + s.name + '].[' + t.name + '] DROP CONSTRAINT [' + fk.name + '];' + CHAR(13)
     FROM sys.foreign_keys fk
     INNER JOIN sys.tables t ON fk.parent_object_id = t.object_id
@@ -18,7 +21,9 @@ BEGIN
     EXEC sp_executesql @sql;
     SET @sql = N'';
 
-    --Eliminar CHECK y DEFAULT constraints
+    -------------------------------------------------------------------
+    -- 2️⃣ Eliminar CHECK y DEFAULT constraints
+    -------------------------------------------------------------------
     SELECT @sql = @sql + 'ALTER TABLE [' + s.name + '].[' + t.name + '] DROP CONSTRAINT [' + c.name + '];' + CHAR(13)
     FROM sys.check_constraints c
     INNER JOIN sys.tables t ON c.parent_object_id = t.object_id
@@ -34,7 +39,56 @@ BEGIN
     EXEC sp_executesql @sql;
     SET @sql = N'';
 
-    --Eliminar todas las tablas del schema
+    -------------------------------------------------------------------
+    -- 3️⃣ Eliminar todos los triggers de tablas del schema
+    -------------------------------------------------------------------
+    SELECT @sql = @sql + 'DROP TRIGGER [' + s.name + '].[' + tr.name + '];' + CHAR(13)
+    FROM sys.triggers tr
+    INNER JOIN sys.tables t ON tr.parent_id = t.object_id
+    INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+    WHERE s.name = @SchemaName
+      AND tr.is_ms_shipped = 0;
+
+    EXEC sp_executesql @sql;
+    SET @sql = N'';
+
+    -------------------------------------------------------------------
+    -- 4️⃣ Eliminar todas las vistas
+    -------------------------------------------------------------------
+    SELECT @sql = @sql + 'DROP VIEW [' + s.name + '].[' + v.name + '];' + CHAR(13)
+    FROM sys.views v
+    INNER JOIN sys.schemas s ON v.schema_id = s.schema_id
+    WHERE s.name = @SchemaName;
+
+    EXEC sp_executesql @sql;
+    SET @sql = N'';
+
+    -------------------------------------------------------------------
+    -- 5️⃣ Eliminar todos los procedimientos almacenados
+    -------------------------------------------------------------------
+    SELECT @sql = @sql + 'DROP PROCEDURE [' + s.name + '].[' + p.name + '];' + CHAR(13)
+    FROM sys.procedures p
+    INNER JOIN sys.schemas s ON p.schema_id = s.schema_id
+    WHERE s.name = @SchemaName;
+
+    EXEC sp_executesql @sql;
+    SET @sql = N'';
+
+    -------------------------------------------------------------------
+    -- 6️⃣ Eliminar todas las funciones (scalar, table-valued, aggregate)
+    -------------------------------------------------------------------
+    SELECT @sql = @sql + 'DROP FUNCTION [' + s.name + '].[' + f.name + '];' + CHAR(13)
+    FROM sys.objects f
+    INNER JOIN sys.schemas s ON f.schema_id = s.schema_id
+    WHERE s.name = @SchemaName
+      AND f.type IN ('FN','IF','TF','AF'); -- scalar, inline TVF, table-valued, aggregate
+
+    EXEC sp_executesql @sql;
+    SET @sql = N'';
+
+    -------------------------------------------------------------------
+    -- 7️⃣ Eliminar todas las tablas del schema
+    -------------------------------------------------------------------
     SELECT @sql = @sql + 'DROP TABLE [' + s.name + '].[' + t.name + '];' + CHAR(13)
     FROM sys.tables t
     INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
@@ -43,10 +97,16 @@ BEGIN
     EXEC sp_executesql @sql;
     SET @sql = N'';
 
-    -- Eliminar el schema
+    -------------------------------------------------------------------
+    -- 8️⃣ Eliminar el schema
+    -------------------------------------------------------------------
     SET @sql = 'DROP SCHEMA [' + @SchemaName + '];';
     EXEC sp_executesql @sql;
 
-    PRINT 'Limpieza completada correctamente.';
+    PRINT 'Limpieza completa del schema finalizada correctamente.';
 END;
 GO
+
+
+
+exec p_Limpiar_Estructura_CSC;
