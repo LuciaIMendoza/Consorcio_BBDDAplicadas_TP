c856e038f4ff2a76b23154ab7c0368a3e5f43215
LuciaIMendoza <61562537+LuciaIMendoza@users.noreply.github.com>
2025-11-01 23:37:47 -0300

    Sp creacion de todo
    SP creacion de todo.
    Ejecutar para crear la base de datos, esquema, sp, tablas y correr la importacion iniical de todos los datos

--- SP_Creacion de todo.sql
+++ SP_Creacion de todo.sql
@@ -1,38 +1,65 @@
+--Crea la Base de Datos
+IF NOT EXISTS (
+    SELECT name FROM sys.databases WHERE name = 'AltosSaintJust'
+)
+BEGIN
+    CREATE DATABASE AltosSaintJust;
+END
+GO
+
 USE AltosSaintJust
 
+--En el menu Query tildar la opcion SQLCMDMODE para poder correr los script de creacion de sp
+
+--Ejecuta la creacion del sp de estructura de tablas
+:r C:\consorcios\StoreProcedureCreacion.sql
+
+--Llama al sp que crea la estructura de tablas
 exec p_Crear_Estructura_CSC;
 
-INSERT INTO csc.Consorcio (nombre, direccion, superficieM2Total) VALUES ('Azcuenaga', 'Belgrano 3344', 1281.00), 
-('Alzaga', 'Callao 1122', 914.00), ('Alberdi', 'Santa Fe 910', 784.00), 
-('Unzue', 'Corrientes 5678', 1316.00), 
-('Pereyra Iraola', 'Rivadavia 1234', 1691.00);
+--Ejecuta la creacion de los SP de importacion
+--Importacion de consorcios
+:r C:\consorcios\StoreProcedureConsorcios.sql
+--Importacion UF
+:r C:\consorcios\StoreProcedureImportarUF.sql
+--Imprtacion CBU
+:r C:\consorcios\StoreProcedureImportarCBUCVU.sql
+--Importar Inquilinos y propietarios
+:r C:\consorcios\StoreProcedureImportarInqPro.sql
+
+----------Importacion de datos --------
+--Ejecuta el sp de Importacion de datos de consorcios
+exec csc.p_ImportarConsorcios @RutaArchivo = 'C:\consorcios\datos varios.xlsx', @Hoja = 'Consorcios'
 
 select * from csc.Consorcio
 
-
+--Ejecuta el SP de Importacion de datos de Unidades Funcionales
 EXEC csc.p_ImportarUnidadFuncional 
      @RutaArchivo = 'C:\consorcios\UF por consorcio.txt';
 
 select * from csc.Unidad_Funcional
 
+--Ejecuta el SP de Importacion de relaciones entre Unidades Funcionales e Inquilinos/Propietarios
 EXEC csc.p_ImportarCBU @RutaArchivo = N'C:\consorcios\Inquilino-propietarios-UF.csv';
 
 select * from csc.Unidad_Funcional
 
 
+--Ejecuta el SP de Importacion de datos de Inquilinos y propietarios
 exec csc.p_ImportarPersonas @RutaArchivo = 'C:\consorcios\Inquilino-propietarios-datos.csv';
 select * from csc.Inquilino;
 select * from csc.Propietario;  
 
-exec  csc.p_ImportarGastos @RutaArchivo = 'C:\consorcios\Servicios.Servicios.json';
+--Ejecuta el SP de Importacion de datos de los Gastos de los consorcios
+--exec  csc.p_ImportarGastos @RutaArchivo = 'C:\consorcios\Servicios.Servicios.json';
 
 
 
 
 
-DELETE FROM CSC.CONSORCIO
-DELETE FROM CSC.UNIDAD_FUNCIONAL 
-DELETE FROM CSC.INQUILINO
-DELETE FROM CSC.PROPIETARIO
+--DELETE FROM CSC.CONSORCIO
+--DELETE FROM CSC.UNIDAD_FUNCIONAL 
+--DELETE FROM CSC.INQUILINO
+--DELETE FROM CSC.PROPIETARIO
 
 
--- StoreProcedureConsorcios.sql
+++ StoreProcedureConsorcios.sql
@@ -1,13 +1,13 @@
 ﻿USE AltosSaintJust;
 GO
 
----------- se Ejecuta esto para habilitar los comandos OPENROWSET --------
-EXEC sp_configure 'show advanced options', 1;
-RECONFIGURE;
-EXEC sp_configure 'Ad Hoc Distributed Queries', 1;
-RECONFIGURE;
-EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1;
-EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1;
+------------ se Ejecuta esto para habilitar los comandos OPENROWSET --------
+--EXEC sp_configure 'show advanced options', 1;
+--RECONFIGURE;
+--EXEC sp_configure 'Ad Hoc Distributed Queries', 1;
+--RECONFIGURE;
+--EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1;
+--EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1;
 
 CREATE OR ALTER PROCEDURE csc.p_ImportarConsorcios
     @RutaArchivo NVARCHAR(500),
@@ -30,6 +30,15 @@ BEGIN
             [Cant unidades funcionales] INT,
             [m2 totales] DECIMAL(10,2)
         );
+
+		---------- se Ejecuta esto para habilitar los comandos OPENROWSET --------
+		EXEC sp_configure 'show advanced options', 1;
+		RECONFIGURE;
+		EXEC sp_configure 'Ad Hoc Distributed Queries', 1;
+		RECONFIGURE;
+		EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1;
+		EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1;
+
 
         SET @Consulta = 'SELECT * FROM [' + @Hoja + '$]';
 
